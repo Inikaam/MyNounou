72741c12a92d302968c2038e085c86d3c2ae03b1
Inikaam <inikaam@gmail.com>
2015-11-03 12:16:11 +0100

    Créer une nanny, partie 2

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -25,6 +25,9 @@
 		})
 		.constant('API_URL', 'http://localhost:8080')
 		.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+			$httpProvider.defaults.useXDomain = true;
+			delete $httpProvider.defaults.headers.common['X-Requested-With'];
+			$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 			$stateProvider
 				.state('authenticate', {
 					url : '/authenticate',
