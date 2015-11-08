338e624620b84668ae4f621173ec72c09566231b
Maxime Martin <contact@maximemartin.fr>
2015-11-08 13:54:04 +0100

    From Slim

--- myApp/platforms/android/assets/www/js/controllers/calendar.js
+++ myApp/platforms/android/assets/www/js/controllers/calendar.js
@@ -15,24 +15,52 @@
 		calendar.dispos = [];
 		
 		// Methods
-		$ionicLoading.show({template: 'Chargement...'});
-		$http({
-			method: 'GET',
-			url: API_URL + '/api/nannies/' + sessionStorage.getItem('user_id') + '?token=' + sessionStorage.getItem('token'),
-		})
-		.success(function(res) {
-			$ionicLoading.hide();
-			if(! res.success)
-				$cordovaToast.show(res.message, 'short', 'bottom');
-			else {
-				calendar.dispos = res.data.dispos;
-				calendar.restrictions = res.data.restrictions;
-			}
-		})
-		.error(function(err) {
-			$ionicLoading.hide();
-			console.error(err);
-		});
+		calendar.removeRestriction = removeRestriction;
+		
+		loadCalendar();
+		
+		
+		function removeRestriction(id) {
+			$ionicLoading.show({template: 'Chargement...'});
+			$http({
+				method: 'DELETE',
+				url: API_URL + '/api/nannies/' + sessionStorage.getItem('user_id') + '/restrictions/' + id + '?token=' + sessionStorage.getItem('token'),
+			})
+			.success(function(res) {
+				$ionicLoading.hide();
+				if(! res.success)
+					$cordovaToast.show(res.message, 'short', 'bottom');
+				else {
+					loadCalendar();
+					$cordovaToast.show(res.message, 'short', 'bottom');
+				}
+			})
+			.error(function(err) {
+				$ionicLoading.hide();
+				console.error(err);
+			});
+		}
+		
+		function loadCalendar() {
+			$ionicLoading.show({template: 'Chargement...'});
+			$http({
+				method: 'GET',
+				url: API_URL + '/api/nannies/' + sessionStorage.getItem('user_id') + '?token=' + sessionStorage.getItem('token'),
+			})
+			.success(function(res) {
+				$ionicLoading.hide();
+				if(! res.success)
+					$cordovaToast.show(res.message, 'short', 'bottom');
+				else {
+					calendar.dispos = res.data.dispos;
+					calendar.restrictions = res.data.restrictions;
+				}
+			})
+			.error(function(err) {
+				$ionicLoading.hide();
+				console.error(err);
+			});
+		}
 	}
 	
 	CalendarEditDayCtrl.$inject = ['$state', '$ionicHistory', '$stateParams', '$cordovaToast', '$http', '$ionicLoading', 'API_URL'];
@@ -130,11 +158,186 @@
 		}
 	}
 	
-	CalendarEditRestrictionCtrl.$inject = ['$state', '$ionicHistory', '$stateParams', '$cordovaToast', '$http', '$ionicLoading', 'API_URL'];
+	CalendarEditRestrictionCtrl.$inject = ['$state', '$filter', '$ionicHistory', '$stateParams', '$cordovaToast', '$http', '$ionicLoading', 'API_URL'];
 	
-	function CalendarEditRestrictionCtrl($state, $ionicHistory, $routeParams, $cordovaToast, $http, $ionicLoading, API_URL) {
+	function CalendarEditRestrictionCtrl($state, $filter, $ionicHistory, $routeParams, $cordovaToast, $http, $ionicLoading, API_URL) {
 		var editRestrict = this;
 		
+		// Variables
+		editRestrict.restrictData = {
+			timeStart: 0,
+			timeEnd: 0,
+			reason: ''
+		}
+		
+		var weekDaysList = ["S", "D", "L", "Ma", "Me", "J", "V"];
+		var monthList = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+		
+		// Methods
+		editRestrict.timePickerCallback = function(){};
+		editRestrict.saveRestrict = saveRestrict;
+		
+		editRestrict.startDatePicker = {
+		    titleLabel: 'Choisir une date',  // Optional
+		    todayLabel: 'Aujourd\'hui',  // Optional
+		    closeLabel: 'Fermer',  // Optional
+		    setLabel: 'Valider',  // Optional
+		    setButtonType : 'button-positive',  // Optional
+		    todayButtonType : 'button-stable hide',  // Optional
+		    closeButtonType : 'button-calm',  // Optional
+		    inputDate: new Date(),  // Optional
+		    mondayFirst: true,  // Optional
+		    templateType: 'popup', // Optional
+		    showTodayButton: false, // Optional
+		    weekDaysList: weekDaysList,
+		    monthList: monthList,
+		    callback: function (val) {  // Mandatory
+		    	if(val)
+		    		editRestrict.startDatePicker.inputDate = val;
+		    },
+		    dateFormat: 'dd/MM/yyyy', // Optional
+		    closeOnSelect: false, // Optional
+		};
+		
+		editRestrict.endDatePicker = {
+				titleLabel: 'Choisir une date',  // Optional
+				todayLabel: 'Aujourd\'hui',  // Optional
+				closeLabel: 'Fermer',  // Optional
+				setLabel: 'Valider',  // Optional
+				setButtonType : 'button-positive',  // Optional
+			    todayButtonType : 'button-stable hide',  // Optional
+			    closeButtonType : 'button-calm',  // Optional
+				inputDate: new Date(),  // Optional
+				mondayFirst: true,  // Optional
+				templateType: 'popup', // Optional
+				showTodayButton: false, // Optional
+				weekDaysList: weekDaysList,
+				monthList: monthList,
+				callback: function (val) {  // Mandatory
+					if(val)
+						editRestrict.endDatePicker.inputDate = val;
+				},
+				dateFormat: 'dd/MM/yyyy', // Optional
+				closeOnSelect: false, // Optional
+		};
+		
+		if($routeParams.id && $routeParams.id != 'new') {
+			
+			$ionicLoading.show({template: 'Chargement...'});
+			$http({
+				method: 'GET',
+				url: API_URL + '/api/nannies/' + sessionStorage.getItem('user_id') + '/restrictions/' + $routeParams.id + '?token=' + sessionStorage.getItem('token'),
+			})
+			.success(function(res) {
+				$ionicLoading.hide();
+				if(! res.success)
+					$cordovaToast.show(res.message, 'short', 'bottom');
+				else {
+					var data = convertDateToData(res.data);
+					editRestrict.startDatePicker.inputDate = data.dateStart;
+					editRestrict.endDatePicker.inputDate = data.dateEnd;
+					editRestrict.restrictData.timeStart = data.timeStart;
+					editRestrict.restrictData.timeEnd = data.timeEnd;
+					editRestrict.restrictData.reason = res.data.reason;
+				}
+			})
+			.error(function(err) {
+				$ionicLoading.hide();
+				console.error(err);
+			});
+		}
+		
+		function saveRestrict() {
+			var start = convertToDate(editRestrict.startDatePicker.inputDate, editRestrict.restrictData.timeStart);
+			var end = convertToDate(editRestrict.endDatePicker.inputDate, editRestrict.restrictData.timeEnd);
+			
+			if(start >= end) {
+				$cordovaToast.show('La date de début ne peut pas être supérieure à la date de fin', 'short', 'bottom');
+			} else if(editRestrict.restrictData.reason == '') {
+				$cordovaToast.show('La raison de l\'indisponibilité doit être indiquée.', 'short', 'bottom');
+			} else {
+				if($routeParams.id == 'new') {
+					$ionicLoading.show({template: 'Chargement...'});
+					$http({
+						method: 'POST',
+						url: API_URL + '/api/nannies/' + sessionStorage.getItem('user_id') + '/restrictions?token=' + sessionStorage.getItem('token'),
+						data: $.param({
+							start: start,
+							end: end,
+							reason: editRestrict.restrictData.reason
+						})
+					})
+					.success(function(res) {
+						$ionicLoading.hide();
+						if(! res.success)
+							$cordovaToast.show(res.message, 'short', 'bottom');
+						else {
+							$ionicHistory.goBack();
+							$cordovaToast.show(res.message, 'short', 'bottom');
+						}
+					})
+					.error(function(err) {
+						$ionicLoading.hide();
+						console.error(err);
+					});
+				} else {
+					$ionicLoading.show({template: 'Chargement...'});
+					$http({
+						method: 'PUT',
+						url: API_URL + '/api/nannies/' + sessionStorage.getItem('user_id') + '/restrictions/' + $routeParams.id + '?token=' + sessionStorage.getItem('token'),
+						data: {
+							start: start,
+							end: end,
+							reason: editRestrict.restrictData.reason
+						}
+					})
+					.success(function(res) {
+						$ionicLoading.hide();
+						console.info(res);
+						if(! res.success)
+							$cordovaToast.show(res.message, 'short', 'bottom');
+						else {
+							$ionicHistory.goBack();
+							$cordovaToast.show(res.message, 'short', 'bottom');
+						}
+					})
+					.error(function(err) {
+						$ionicLoading.hide();
+						console.error(err);
+					});
+				}
+			}
+		}
+		
+		function convertToDate(date, time) {
+			var year = $filter('date')(date, 'yyyy');
+			var month = parseInt($filter('date')(date, 'MM'), 10) - 1;
+			var day = $filter('date')(date, 'dd');
+			var hour = Math.floor(time / 3600);
+			var min = Math.floor((time % 3600) / 60);
+			
+			return new Date(year, month, day, hour, min);
+		}
+		
+		function convertDateToData(date) {
+			var data = {};
+			
+			data.dateStart = date.start;
+			data.dateEnd = date.end;
+			
+			data.timeStart = extractTime(date.start);
+			data.timeEnd = extractTime(date.end);
+			
+			console.info(data);
+			return data;
+		}
+		
+		function extractTime(date) {
+			var hour = parseInt($filter('date')(date, 'HH'));
+			var min = parseInt($filter('date')(date, 'mm'));
+			
+			return ((hour * 3600) + (min * 60));
+		}
 		
 	}
 
--- myApp/platforms/android/assets/www/js/controllers/parents.js
+++ myApp/platforms/android/assets/www/js/controllers/parents.js
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+(function() {
+	'use strict';
+
+	angular.module('myNounou')
+		.controller('NanniesSearchCtrl', NanniesSearchCtrl);
+
+
+	
+	NanniesSearchCtrl.$inject = ['$state', '$stateParams', '$cordovaToast', '$http', '$ionicLoading', 'API_URL'];
+	
+	function NanniesSearchCtrl($state, $routeParams, $cordovaToast, $http, $ionicLoading, API_URL) {
+		var searchNannies = this;
+
+		searchNannies.slots = { epochTime: 12600, format: 24, step: 15 };
+		searchNannies.slots2 = { epochTime: 12600, format: 24, step: 15 };
+
+		searchNannies.timePickerCallback = function (val) {
+		  if (typeof (val) === 'undefined') {
+		    console.log('Time not selected');
+		  } else {
+		    console.log('Selected time is : ', val);    // `val` will contain the selected time in epoch
+		  }
+		}
+	};
+	
+=======
 (function() {
 	'use strict';
 
@@ -29,4 +56,5 @@
 		var profileParents = this;
 	}
 	
+>>>>>>> origin/slim
 })();
